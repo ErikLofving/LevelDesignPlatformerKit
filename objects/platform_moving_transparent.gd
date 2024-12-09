0f19000e73efc1cb29bd3da2abd4f8d9a995e903
ErikLofving <144666766+ErikLofving@users.noreply.github.com>
2024-12-09 20:02:59 +0100

    [ADD]
    Added some more environment, changed ending section, some stuff still needs fixing.

---
+++ objects/platform_moving_transparent.gd
@@ -0,0 +1,29 @@
+extends Node3D
+
+@onready var playerCharacter : Node3D = get_tree().get_first_node_in_group("PlayerGroup")
+
+@export var opacity : float = 0.5
+
+@onready var platform_falling_2 = $"platform-falling2"
+
+@onready var animation_player = $AnimationPlayer
+
+var transparencyChange : float
+
+
+func _on_area_3d_body_entered(body):
+	if body == playerCharacter:
+		
+		animation_player.play("Transparent")
+		print("player has entered the area!")
+	
+	pass # Replace with function body.
+
+
+func _on_area_3d_body_exited(body):
+	if body == playerCharacter:
+		
+		animation_player.play("Opacity")
+		print("player has entered the area!")
+	
+	pass # Replace with function body.
